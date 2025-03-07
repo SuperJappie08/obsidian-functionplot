e6798d4c9f14502bd7a385fa9abca5ea20c909c6
Leonhard Masche <leonhardmasche@gmail.com>
2025-03-07 18:27:11 +0100

    add flake for development on nixos, track lockfiles

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
+
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  languages.javascript = {
+                    enable = true;
+                    pnpm.enable = true;
+                  };
+                  languages.typescript.enable = true;
+                }
+              ];
+            };
+          });
+    };
+}
